fa2acd297247ed25289239e270299dfb24902965
Mik@ <michael.ledour@gmail.com>
2017-08-12 16:17:46 +0200

    Initial commit

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { SampleModule } from './src';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SampleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
